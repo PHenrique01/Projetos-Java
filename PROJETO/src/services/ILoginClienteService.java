35259679819204c333f420f51a807b1ed0f066f8
PHenrique01 <paulinho.pawer14@gmail.com>
2023-06-14 14:01:57 -0300

    Atualização do projeto java - 04

--- PROJETO/src/services/ILoginClienteService.java
+++
@@ -1,5 +0,0 @@
-package services;
-
-public interface ILoginClienteService {
-	public boolean autenticar(String email, String senha);
-}
--- PROJETO/src/services/IRealizarSeguro1Service.java
+++
@@ -1,5 +0,0 @@
-package services;
-
-public interface IRealizarSeguro1Service {
-	public boolean realizarSeguro(String nome, String email, String cep, String cpf, String dataNasc, String contato, String inicioVigencia, String fimVigencia);
-}
